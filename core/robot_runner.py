3c89926c6a8a8231a29330deecf927fff0cdbcf5
Caíque Araújo Spósito <caique.sposito@icts.com.br>
2021-03-04 11:41:17 -0300

    fix: env var app_path

--- core/robot_runner.py
+++ core/robot_runner.py
@@ -4,6 +4,7 @@ from xml.etree.ElementTree import parse as parse_xml
 
 from robot.run import run
 
+from .env import env
 from .logger import get_logger
 from .exceptions import TaskFailedException
 
@@ -21,17 +22,16 @@ def parse_result(file):
   return result.text if result is not None else None
 
 def exec_robot(subject: str, related_data: Optional[dict] = None):
-  cwd = os.getcwd()
   logger = get_logger()
 
   # Parsea o JSON de related_data para as variáveis da execução das tarefas
   variables = [''] if related_data is None else list(map(lambda kv: '{0}:{1}'.format(kv, related_data[kv]), related_data))
 
   # Gera o nome do arquivo de output
-  output_file = '{}/robot/results/{}.xml'.format(cwd, time.time())
+  output_file = '{}robot/results/{}.xml'.format(env('APP_PATH'), time.time())
 
   logger.info('\tRobot task started...')
-  run('{}/crawlers/{}.robot'.format(cwd, subject), output=output_file, log=None, report=None, console='quiet', variable=variables)
+  run('{}crawlers/{}.robot'.format(env('APP_PATH'), subject), output=output_file, log=None, report=None, console='quiet', variable=variables)
   logger.info('\t... robot task finished')
   result = parse_result(output_file)
   result = json.loads(result) if result is not None else None
