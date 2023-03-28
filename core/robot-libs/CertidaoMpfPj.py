bfea7993f1742bd3e0d6e53c68b1a11b6474ca85
viniciuslb-icts <vinicius.barbosa@icts.com.br>
2023-03-28 11:34:07 -0300

    feature: create crawler

---
+++ core/robot-libs/CertidaoMpfPj.py
@@ -0,0 +1,79 @@
+from core.env import env
+from utils import write_results
+from robot.api.logger import console
+from robot.api.deco import keyword, library
+from anticaptchaofficial.recaptchav2proxyless import *
+
+import os
+import re
+import json
+import base64
+import requests
+import NewCoreLib
+
+
+@library(scope='GLOBAL', version='0.0.1')
+class CertidaoMpfPj(NewCoreLib.NewCoreLib):
+    @keyword('Certidao MPF - PJ')
+    def certificate_mpf_pj(self, cnpj: str):
+        try:
+            data = {}
+            cnpj_formatted = re.sub('[^0-9]', '', cnpj)
+            website_key = '6LeUowITAAAAAOIiAB441SS3EF77AS4ZuK0LFsaH'
+            pdf_file_name = 'certidao_mpf_pj_' + cnpj_formatted + '.pdf'
+            url = 'https://aplicativos.mpf.mp.br/ouvidoria/app/cidadao/certidao'
+            api_url = 'https://aplicativos.mpf.mp.br/ouvidoria/rest/v1/publico/certidao'
+
+            console('Resolvendo recaptcha...')
+            solver = recaptchaV2Proxyless()
+            solver.set_verbose(1)
+            solver.set_key(env('CAPTCHA_KEY'))
+            solver.set_website_url(url)
+            solver.set_website_key(website_key)
+            g_response = solver.solve_and_return_solution()
+            console('Recaptcha resolvido')
+
+            headers = {
+                "Content-Type": "application/x-www-form-urlencoded"
+            }
+
+            url_params = '/emitir?documento=' + \
+                str(cnpj_formatted) + '&recaptcha=' + \
+                str(g_response) + '&tipoPessoa=J'
+
+            full_request = str(api_url) + str(url_params)
+
+            try:
+                response = requests.get(full_request, headers=headers)
+                console('Requisição para pegar o data id...')
+
+                content = json.loads(response.content)
+
+                dataId = content['data']
+
+                url_to_download_pdf = str(api_url) + '/download/' + str(dataId)
+
+                response = requests.get(url_to_download_pdf)
+                console('Baixando pdf...')
+
+                with open(pdf_file_name, 'wb') as pdf_file:
+                    pdf_file.write(response.content)
+                    console('Criando um novo pdf...')
+
+                with open(pdf_file_name, 'rb') as pdf_file:
+                    evidence_b64 = base64.b64encode(pdf_file.read())
+                    console('Convertendo o pdf para base64...')
+
+                data['evidence'] = 'data:application/pdf;base64,{}'.format(
+                    evidence_b64)
+                data['found'] = True
+                data['evidence_type'] = 'pdf'
+
+            except requests.exceptions.RequestException as e:
+                data['found'] = False
+
+            console('Remover o pdf...')
+            os.remove(pdf_file_name)
+            write_results(json.dumps(data, ensure_ascii=False))
+        except Exception as e:
+            raise Exception('resultado fora do esperado: Erro: ', e)
