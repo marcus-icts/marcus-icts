dd6d66a4186e403832ffc086b7393c74e068c3f1
Kaliary Cisne de Sousa da Silva <kaliary.silva@icts.com.br>
2023-04-05 10:12:34 -0300

    fix: no attribute page

--- core/robot-libs/QuitacaoEleitoral.py
+++ core/robot-libs/QuitacaoEleitoral.py
@@ -16,7 +16,7 @@ class QuitacaoEleitoral(NewCoreLib.NewCoreLib):
             nome_mae = nome_mae if nome_mae != '' else 'NAO CONSTA'
             data = {}
             textNaoQuite = "abaixo qualificado(a) não está quite"
-
+            url_site = 'https://www.tse.jus.br/servicos-eleitorais/certidoes/certidao-de-quitacao-eleitoral'
             url = 'https://www.tse.jus.br/@certidao-quitacao'
             body = {
                 'token':'03AFY_a8Xk9JUcRm6eavVbw9DAeiWz1UaTDFckDBFPxeLjKMVwThJOBKkQySNjzdDERS_1nZ-EMt2iOjTOic_aiyJvLc_SqSkdE3tjzQNi8MBXLDAlAZWQhR9W3blyXqNFHz3FS6fCe7tf_2SeqgyBb6DJyRTVRbYUc4z7AOFdbaxuLqqk0Cp51xdVD9NVtIfXv5ul26ouXgZe2MjBPtrEWqC8ICf_whQXS4oYOvBzMMoCvg48QGPFqQtbYh4LkIUVzi2X4IYUW5BMJcdxa4fkwV15Vt3SeOEzxYVcNaH-Id1FTdZIW-hMO2v4tX0gqPj45JWPW0T7rz4dV4yYxU88vjTlfSBZJny-TFGwjAVKC3ntPK66WdfUKd3CqyntpAvmWAUiwmQWXyIuyuFG57mhxCtQn_fOr9ag4mC308uVQrL_nw3WRgjyiikrlxsh28ZeLLPzQWHcYDuMpTW5zRKEeGXsP37F2DDPH6HhMTSZN5xnISRmrMlT_qIHRLFEdPoBpZ_PWQY_bvGi8hESXkt8Yh2QT2A15SVrmL2IAFjgAYgyDGctC_Rq2pb1n_Z7dq9i5GNJndIF2aPP_Gc84cSQ8EAMFZrb7IP_xc9v3DOOtu_ReH-9QB5RkkZ7mwD126QXs0DVIUIBkiQNdD5TwwH5D3LrpfK1x8-oTq02O2UBD-cOf0NrHcy92MUCwWqX0V1jywB7QOoqPOrPbGWExg4jrlLFIsVKU1sLS5Cu6SqqT6XR50juNNzqWetrVI7oARIwXHcsvI5cmFUh1WkNZkVbCCzumRpmMuit8Tq7YfT5-BaUM52TGCNTB3qS8AYilGbIQeg0P4_tVHFph-P1qAL9SnQy3CySQhVqJzmwRo75VX8ghwVhrhx7kEddmqsvzca-6OV_2dADNoh8uKQtEVBhDQZY9dm9Q76jTwBlWiTFqbUnIuLrUqzbPNA2vpYBFFZukAhu0C2ETvPTUoGzbt3LiXlAldG56-jZnMDp7EkN2Um1GbIS2rEoYRUXCGiR9t8stDky7-3SH6Xpjs4_ljgMgkBZlJHyT_b70dCdueTuT-K26co7dqLAzo5n7sW4fegvJ1RzCICCzhtWjXfUNFqR6im3pHRBA_u1dLBfhS9WEj_swe-L-R9PXgYQUKCuDGL3LzAAwgjj0vvoeVPyR5RQtS8T-NCiSbZRejV6ao08D2Iv6Kr9tZ2Dvs669PyM_HtJ3Rp40Z6C7aWeHU6IKW4Rdvhw1T8Ux0RvncDTVCJOPCwtr5q95xLdTamo5EQgrk9C3HiWaQZ5w3-lNy8pyG-21zJGqGj10zM-HjuOcVM0-cZQO8yqN80cmdS3prbBDTKW3tg-sn-Z44pyM6p8LXiW-zR0JZD_PTZX40eZVkPtYt4aqoKCbub5g4uQ7mnIh4OXF3PQ82T9Pehy',
@@ -59,6 +59,7 @@ class QuitacaoEleitoral(NewCoreLib.NewCoreLib):
                 data['found'] = True
                 self.teardown()
             else:
+                self.open_browser(url_site)
                 raise Exception(content['message'])
 
             write_results(json.dumps(data, ensure_ascii=False))
@@ -105,5 +106,4 @@ class QuitacaoEleitoral(NewCoreLib.NewCoreLib):
             self.page.evaluate("document.querySelector('#modal-lgpd').remove()")
             self.wait_sleep(2)
         except:
-            self.teardown()
             raise Exception('Problema na navegação do site.')
