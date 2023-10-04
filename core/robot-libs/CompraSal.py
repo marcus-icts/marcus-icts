685a02b7b7b3fff91c3c6ecbc806a0ce05041e2f
Rafael Neri <rafael.neri@icts.com.br>
2023-10-04 10:49:41 -0300

    Add verificações

--- core/robot-libs/CompraSal.py
+++ core/robot-libs/CompraSal.py
@@ -8,27 +8,27 @@ from robot.api.logger import console, error
 import NewCoreLib
 
 from time import sleep
-from requests.exceptions import Timeout, RequestException
+from requests.exceptions import Timeout
 from utils import write_results
 
 
-@library(scope='GLOBAL', version='0.0.1')
+@library(scope="GLOBAL", version="0.0.1")
 class CompraSal(NewCoreLib.NewCoreLib):
     # Constantes usadas para o Crawling da API
     PAGE_URL = "https://unac.mh.gob.sv/comprasalweb/proveedores"
-    API_URL = 'https://unacv2.mh.gob.sv/comprasalmicro/portalpublico/api/v1'
+    API_URL = "https://unacv2.mh.gob.sv/comprasalmicro/portalpublico/api/v1"
     USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"
     API_PAGE_SIZE = 50
     API_HEADERS = {
-        'Accept': 'application/json, text/plain, */*',
-        'Accept-Language': 'en-US,en;q=0.9',
-        'Content-Type': 'application/json',
-        'Origin': 'https://unac.mh.gob.sv',
-        'Referer': 'https://unac.mh.gob.sv/',
-        'User-Agent': USER_AGENT,
-        'sec-ch-ua': '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Linux"'
+        "Accept": "application/json, text/plain, */*",
+        "Accept-Language": "en-US,en;q=0.9",
+        "Content-Type": "application/json",
+        "Origin": "https://unac.mh.gob.sv",
+        "Referer": "https://unac.mh.gob.sv/",
+        "User-Agent": USER_AGENT,
+        "sec-ch-ua": '"Google Chrome";v="117", "Not;A=Brand";v="8", "Chromium";v="117"',
+        "sec-ch-ua-mobile": "?0",
+        "sec-ch-ua-platform": '"Linux"',
     }
     DEFAULT_TIMEOUT = 30
     DEFAULT_SLEEP = 60
@@ -36,20 +36,22 @@ class CompraSal(NewCoreLib.NewCoreLib):
 
     def __init__(self):
         """
-        Inicializa o serviço do playwright e executa o navegador (firefox).
+        Inicializa o serviço do playwright e executa o navegador.
         :return: None
         """
         if self.browser is None:
-            self.open_browser("about:blank")
+            try:
+                self.open_browser("about:blank")
+            except Exception:
+                pass  # Evita que o Script quebre se não for possível abrir o navegador
 
-    @keyword('comprasal')
+    @keyword("comprasal")
     def comprasal(self, name: str) -> None:
         """
         Consulta a API do CompraSal (El Salvador).
         :param name: Termo a ser pesquisado na API
         :return: None
         """
-
         # Variavéis de controle
         page = 1
         records = None
@@ -65,9 +67,7 @@ class CompraSal(NewCoreLib.NewCoreLib):
 
             console(f"\nBuscando página {page} de resultados para a consulta {name}")
             search_response = self.make_request(
-                url=f"{self.API_URL}/proveedores",
-                method="POST",
-                data=data
+                url=f"{self.API_URL}/proveedores", method="POST", data=data
             )
 
             if search_response is not None and search_response.status_code == 200:
@@ -76,21 +76,18 @@ class CompraSal(NewCoreLib.NewCoreLib):
 
                 # Obtendo os ids da página atual
                 for row in search_result["content"]:
-                    rows.append({
-                        "id": row["id"],
-                        "nombre": row["nombre"],
-                    })
+                    rows.append(
+                        {
+                            "id": row["id"],
+                            "nombre": row["nombre"],
+                        }
+                    )
             else:
                 error(f"Falhou em buscar dados na página {page}")
 
             page += 1
 
-        if records is None or records == 0:
-            console(f"\nNenhum resultado encontrado")
-        elif records == 1:
-            console(f"\n{records} resultado encontrado")
-        else:
-            console(f"\n{len(rows)} resultados encontrados")
+        self.show_message_total_results(records)
 
         # Buscando os dados de cada id
         for key, row in enumerate(rows):
@@ -104,7 +101,7 @@ class CompraSal(NewCoreLib.NewCoreLib):
 
             if data_response is not None and data_response.status_code == 200:
                 data_result = data_response.json()
-                nomeComercial = str(data_result["nombreComercial"]).upper()
+                nome_comercial = str(data_result["nombreComercial"]).upper()
 
                 provider_details = data_result.copy()
                 del provider_details["nombre"]
@@ -113,19 +110,21 @@ class CompraSal(NewCoreLib.NewCoreLib):
 
                 service_details = data_result["obsList"].copy()
 
-                results.append({
-                    "nombre": current_name,
-                    "comercial": nomeComercial,
-                    "detalles_del_provedor": provider_details,
-                    "bienes_obras_servicos": service_details,
-                    "evidence": self.evidence(f"{self.PAGE_URL}/{current_id}"),
-                })
+                results.append(
+                    {
+                        "nombre": current_name,
+                        "comercial": nome_comercial,
+                        "detalles_del_provedor": provider_details,
+                        "bienes_obras_servicos": service_details,
+                        "evidence": self.evidence(f"{self.PAGE_URL}/{current_id}"),
+                    }
+                )
             else:
                 error(f"Falhou ao obter informações do ID: {id}")
 
-        console(f"\n{records} resultados encontrados.\n\n")
+        self.show_message_total_results(records)
+
         write_results(json.dumps({"data": results}, ensure_ascii=False))
-        # console(json.dumps({"data": results}, ensure_ascii=False, indent=4))
 
     def make_request(self, url, method="GET", data=None) -> requests.Response | None:
         """
@@ -147,22 +146,38 @@ class CompraSal(NewCoreLib.NewCoreLib):
                     url,
                     headers=self.API_HEADERS,
                     json=data,
-                    timeout=self.DEFAULT_TIMEOUT
+                    timeout=self.DEFAULT_TIMEOUT,
                 )
                 response.raise_for_status()
                 break
             except Timeout:
                 attempts += 1
-                error(f"Tempo de espera da solicitação excedido - tentativa {attempts} de {self.MAX_RETRIES}")
+                error(
+                    f"Tempo de espera da solicitação excedido - tentativa {attempts} de {self.MAX_RETRIES}"
+                )
                 sleep(self.DEFAULT_SLEEP)
                 continue
-            except RequestException:
+            except Exception:
                 attempts += 1
-                error(f"Erro na solicitação - tentativa {attempts} de {self.MAX_RETRIES}")
+                error(
+                    f"Erro na solicitação - tentativa {attempts} de {self.MAX_RETRIES}"
+                )
                 sleep(self.DEFAULT_SLEEP)
                 continue
 
         return response
+
+    def show_message_total_results(self, results: int) -> None:
+        """
+        Exibe uma mensagem com o total de resultados
+        :return: None
+        """
+        if results is None or results == 0:
+            console("\nNenhum resultado encontrado")
+        elif results == 1:
+            console("\nUm resultado encontrado")
+        else:
+            console(f"\n{results} resultados encontrados")
 
     def evidence(self, url: str) -> str:
         """
@@ -170,10 +185,15 @@ class CompraSal(NewCoreLib.NewCoreLib):
         :return: Base64 da imagem
         :rtype: str
         """
-
-        self.page.goto(url, timeout=self.DEFAULT_TIMEOUT*1000)
-        self.wait_for_element("body > app-root div.container")
-        return self.take_evidence()
+        if self.browser is not None:
+            try:
+                self.page.goto(url, timeout=self.DEFAULT_TIMEOUT * 1000)
+                self.wait_for_element("body > app-root div.container")
+                return self.take_evidence()
+            except Exception:
+                return ""  # Evita que o Script quebre se não for possível tirar o screenshot
+        else:
+            return ""  # Retorna screenshot vazio se não for possivel abrir o navegador
 
     def __del__(self):
         """
@@ -181,4 +201,7 @@ class CompraSal(NewCoreLib.NewCoreLib):
         :return: None
         """
         if self.browser is not None:
-            self.teardown()
+            try:
+                self.teardown()
+            except Exception:
+                pass  # Evita que o Script quebre se não for possível fechar o navegador
